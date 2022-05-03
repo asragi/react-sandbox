cdcc04633f2ea35a318447d76e8afb5556892630
Ragi <ragi-justaway@hotmail.co.jp>
2022-05-03 15:56:07 +0900

    use mock server

--- src/features/hooks/useFetchPost.test.ts
+++ src/features/hooks/useFetchPost.test.ts
@@ -1,7 +1,12 @@
 import { renderHook } from "@testing-library/react";
 import { useFetchPost } from "./useFetchPost";
+import { server } from '../../mocks';
 
 describe("useFetchPost", () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
   it("should be empty array", () => {
     const { result } = renderHook(() => useFetchPost({}));
     const { current } = result;
